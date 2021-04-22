4c4b8a9da34b93037db98ef2477f3571fb6f7298
eylles <ed.ylles1997@gmail.com>
2021-04-22 03:23:09 -0600

    change bar swap option to -B flag

--- options.c
+++ options.c
@@ -30,7 +30,7 @@ const opt_t *options = (const opt_t*) &_options;
 
 void print_usage(void)
 {
-	printf("usage: sxiv [-abcfhiopqrtvZ] [-A FRAMERATE] [-e WID] [-G GAMMA] "
+	printf("usage: sxiv [-abBcfhiopqrtvZ] [-A FRAMERATE] [-e WID] [-G GAMMA] "
 	       "[-g GEOMETRY] [-N NAME] [-n NUM] [-S DELAY] [-s MODE] [-z ZOOM] "
 	       "FILES...\n");
 }
@@ -73,7 +73,7 @@ void parse_options(int argc, char **argv)
 	_options.clean_cache = false;
 	_options.private_mode = false;
 
-	while ((opt = getopt(argc, argv, "A:abcde:fG:g:hin:N:opqrS:s:tvZz:")) != -1) {
+	while ((opt = getopt(argc, argv, "A:abBce:fG:g:hin:N:opqrS:s:tvZz:")) != -1) {
 		switch (opt) {
 			case '?':
 				print_usage();
@@ -93,7 +93,7 @@ void parse_options(int argc, char **argv)
 			case 'c':
 				_options.clean_cache = true;
 				break;
-			case 'd':
+			case 'B':
 				_options.swap_bar = true;
 				break;
 			case 'e':
